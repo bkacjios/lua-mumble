8a9029aa94a36ccff13da83fabf70f569b0f28d5
Bkacjios <blackops7799@gmail.com>
2020-06-10 14:23:20 -0400

    Added mumble.encoder:getBitRate/encode/encode_float for eventual methods to transmit audio data manually

--- encoder.c
+++ encoder.c
@@ -19,8 +19,7 @@ int mumble_encoder_new(lua_State *l)
 	}
 
 	opus_encoder_ctl(encoder, OPUS_SET_VBR(0));
-	opus_encoder_ctl(encoder, OPUS_SET_BITRATE(40000));
-
+	opus_encoder_ctl(encoder, OPUS_SET_BITRATE(AUDIO_DEFAULT_BITRATE));
 	return 1;
 }
 
@@ -29,6 +28,49 @@ static int encoder_setBitRate(lua_State *l)
 	OpusEncoder *encoder = luaL_checkudata(l, 1, METATABLE_ENCODER);
 	opus_encoder_ctl(encoder, OPUS_SET_BITRATE(luaL_checkinteger(l, 2)));
 	return 0;
+}
+
+static int encoder_getBitRate(lua_State *l)
+{
+	OpusEncoder *encoder = luaL_checkudata(l, 1, METATABLE_ENCODER);
+	opus_int32 rate;
+	opus_encoder_ctl(encoder, OPUS_GET_BITRATE(&rate));
+	lua_pushinteger(l, rate);
+	return 1;
+}
+
+static int encoder_encode(lua_State *l)
+{
+	OpusEncoder *encoder = luaL_checkudata(l, 1, METATABLE_ENCODER);
+	int frames = luaL_checkinteger(l, 2);
+
+	size_t len;
+	const opus_int16* pcm = (opus_int16*) luaL_checklstring(l, 3, &len);
+
+	const uint32_t enc_frame_size = frames * AUDIO_SAMPLE_RATE / 100;
+
+	uint8_t output[0x1FFF];
+	opus_int32 outlen = opus_encode(encoder, pcm, enc_frame_size, output, len);
+
+	lua_pushlstring(l, output, outlen);
+	return 1;
+}
+
+static int encoder_encode_float(lua_State *l)
+{
+	OpusEncoder *encoder = luaL_checkudata(l, 1, METATABLE_ENCODER);
+	int frames = luaL_checkinteger(l, 2);
+
+	size_t len;
+	const float* pcm = (float*) luaL_checklstring(l, 3, &len);
+
+	const uint32_t enc_frame_size = frames * AUDIO_SAMPLE_RATE / 100;
+
+	uint8_t output[0x1FFF];
+	opus_int32 outlen = opus_encode_float(encoder, pcm, enc_frame_size, output, len);
+
+	lua_pushlstring(l, output, outlen);
+	return 1;
 }
 
 static int encoder_tostring(lua_State *l)
@@ -40,6 +82,9 @@ static int encoder_tostring(lua_State *l)
 
 const luaL_Reg mumble_encoder[] = {
 	{"setBitRate", encoder_setBitRate},
+	{"getBitRate", encoder_getBitRate},
+	{"encode", encoder_encode},
+	{"encode_float", encoder_encode_float},
 	{"__tostring", encoder_tostring},
 	{NULL, NULL}
 };
